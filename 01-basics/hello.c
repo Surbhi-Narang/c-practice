aa44ced16ff38c4e0286504f7b0bae398e59b3dc
Surbhi Narang <surbhinarang65@gmail.com>
2026-04-01 03:06:13 +0530

    Final Update hello.c
    Added comments explaining basic C concepts

--- 01-basics/hello.c
+++ 01-basics/hello.c
@@ -3,3 +3,17 @@ int main(){
     printf("Hello World");
     return 0;
 }
+
+// Any C language program starts with the main() function and executes line by line.
+
+// In a C program, the first line inside the main() function (like printf) is executed first, then the next line, and so on.
+
+// Returning from a function means the function has stopped executing.
+
+// #include<stdio.h> is a header file - it is a preprocessor directive. 
+
+// int main() means the main function will return an integer value (usually 0). 
+
+// { } indicate the start and end of the main function.
+
+// In C, ; is used to end statements. printf() is used to print output. Text inside " " is called a string.
